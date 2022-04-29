4ca40578ee2372eaa8ec680046388ee274036386
egfs1 <eduardogomesfs4@gmail.com>
2022-04-28 22:23:03 -0300

    create a handle error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import handleErrors from './errors'
 import router from './routes'
 
 const app = express()
 
 app.use(express.json())
+app.use(handleErrors)
 app.use(router)
 
 const port = process.env.PORT || 3000
