06d39ed17a23e2697d6c55205da87c26bb1b710c
egfs1 <eduardogomesfs4@gmail.com>
2022-06-11 13:01:19 -0300

    fixing migrations

--- prisma/migrations/20220605202725_create_users/migration.sql
+++ prisma/migrations/20220605202725_create_users/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
-CREATE TABLE `Users` (
+CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `isAdmin` BOOLEAN NOT NULL,
 
-    UNIQUE INDEX `Users_name_key`(`name`),
+    UNIQUE INDEX `users_name_key`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20220611153940_change_users_reference/migration.sql
+++
@@ -1,2 +0,0 @@
--- RenameIndex
-ALTER TABLE `users` RENAME INDEX `Users_username_key` TO `users_username_key`;
