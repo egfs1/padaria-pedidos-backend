d018fff04b289f9dcd47786afd747c51ca869194
Eduardo Gomes <eduardogomesfs4@gmail.com>
2023-03-06 12:40:07 -0300

    changed error handling response

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -3,7 +3,8 @@ import { NextFunction, Request, Response} from "express";
 function error(err: Error, request: Request, response: Response, next: NextFunction){
     if (err instanceof Error){
         return response.status(400).json({
-            error: err.message
+            status: "error",
+            message: err.message
         })
     }
     return response.status(500).json({
